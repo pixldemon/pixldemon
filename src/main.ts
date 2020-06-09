e5fb7b4146c8e15e75a87981c8c2aba51671cea6
pixldemon <moritzamando@gmail.com>
2020-06-09 19:32:57 +0200

    Switched analytics to vue-analytics

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Analytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(Analytics, {
+  id: 'UA-168769445-1',
+  router
+})
 
 new Vue({
   router,
